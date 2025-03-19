1530eb9b6cefaed78a724c6744af2b93d76fa6c3
Remijang <wuet0829@gmail.com>
2025-03-19 23:55:19 +0800

    test

--- student_agent.py
+++ student_agent.py
@@ -21,8 +21,12 @@ def get_action(obs):
     # NOTE: Keep in mind that your Q-table may not cover all possible states in the testing environment.
     #       To prevent crashes, implement a fallback strategy for missing keys. 
     #       Otherwise, even if your agent performs well in training, it may fail during testing.
+
+    lis = list(obs)
+    lis[0] %= 5
+    lis[1] %= 5
     
-    return table.get_action(obs)
+    return table.get_action(tuple(lis))
 
     # return random.choice([0, 1, 2, 3, 4, 5]) # Choose a random action
     # You can submit this random agent to evaluate the performance of a purely random strategy.
